829298e314a7a6a7ab1950e6c49bf05b265e635e
Joshua <horheyinc@gmail.com>
2026-05-12 20:58:36 -0500

    random commit message

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const ourChoiceContainer = document.querySelector(".big-choice");
-const compChoiceContainer = document.querySelector(".ai-big-choice");
 const API_KEY = "AIzaSyBHvDXtVyqgie8U4VtuOw6uZo9t-edB7Bg";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${API_KEY}`;
 const requestBody = {
@@ -13,10 +11,6 @@ const requestBody = {
     },
   ],
 };
-const humanChoicesArray = [];
-const ironChoiceElement = document.querySelector("#iron");
-const paperChoiceElement = document.querySelector("#paper");
-const scissorsChoiceElement = document.querySelector("#scissors");
 
 async function getMoveFromAI() {
   try {
@@ -28,64 +22,10 @@ async function getMoveFromAI() {
       body: JSON.stringify(requestBody),
     });
     const data = await res.json();
-    const theActualMove = data.candidates[0].content.parts[0].text;
-    console.log("here is the move", theActualMove);
-    return theActualMove;
+    const AIsMove = data.candidates[0].content.parts[0].text;
+    console.log("here is the move", AIsMove);
+    return AIsMove;
   } catch (error) {
     console.log(error);
   }
 }
-
-//event listeners
-ironChoiceElement.addEventListener("click", async () => {
-  const ourImg = document.createElement("img");
-  ourImg.src = "./images/iron.png";
-  ourImg.alt = "iron picture";
-  ourImg.classList.add("our-big-choice");
-  ourChoiceContainer.appendChild(ourImg);
-
-  const computerChoice = await getMoveFromAI();
-  if (computerChoice === "Scissors") {
-    console.log("comp choose scissors ");
-    const compImg = document.createElement("img");
-    compImg.src = "./images/scissors.jpg";
-    compImg.alt = "scissors picture";
-    compImg.classList.add("our-big-choice");
-    compChoiceContainer.appendChild(compImg);
-
-    //alert the player who won
-    setTimeout(() => {
-      alert("Iron beats Scissors, you win!!!");
-      compChoiceContainer.innerHTML = "";
-      ourChoiceContainer.innerHTML = "";
-    }, 1000);
-  } else if (computerChoice === "Rock") {
-    console.log("comp choose rock ");
-    const compImg = document.createElement("img");
-    compImg.src = "./images/iron.png";
-    compImg.alt = "iron picture";
-    compImg.classList.add("our-big-choice");
-    compChoiceContainer.appendChild(compImg);
-
-    //alert the player who won
-    setTimeout(() => {
-      alert("Iron vs Iron, its a tie :)");
-      compChoiceContainer.innerHTML = "";
-      ourChoiceContainer.innerHTML = "";
-    }, 1000);
-  } else {
-    console.log("comp choose paper ");
-    const compImg = document.createElement("img");
-    compImg.src = "./images/paper.png";
-    compImg.alt = "paper picture";
-    compImg.classList.add("our-big-choice");
-    compChoiceContainer.appendChild(compImg);
-
-    //alert the player who won
-    setTimeout(() => {
-      alert("Paper Beats Iron, AI wins");
-      compChoiceContainer.innerHTML = "";
-      ourChoiceContainer.innerHTML = "";
-    }, 1000);
-  }
-});
